c7e49c8bbff5590c0b57ba17cf180c6ec8d66301
EVCAtecc <62038077+EVCAtecc@users.noreply.github.com>
2020-03-12 19:57:56 -0600

    Updated dist2pvc and servo position
    This was done to account for the appropriate sized cup.

--- cupSliderMain.c
+++ cupSliderMain.c
@@ -1,8 +1,4 @@
 #include <kipr/wombat.h>
-/*
-move backwards for about 2 seconds, then rotate left and continue to rotate and when sensor input is <1 rotate for
-about 8-10 seconds then drive up ramp and use line follwoing program (:
-*/
 
 int motorL = 0;
 int motorR = 1;
@@ -12,20 +8,14 @@ int tail = 1;
 int rangeS = 0;
 int touch = 2;
 
-int dist2pvc = 2000;
+int dist2pvc = 1400;
 
 int main()
 {
     printf("Hello World\n");
     
     enable_servos(); 
-    set_servo_position(tail, 845);
-    
-    //turn to angle towards the ramp
-    /*
-    motor(motorL, 50); 
-    motor(motorR, -50); 
-    msleep(700); */ 
+    set_servo_position(tail, 990);
     
     //go forward until close enough to pvc
     while (analog(rangeS)<= dist2pvc){
